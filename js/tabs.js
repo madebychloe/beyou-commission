be71c60360433a44b7c412133990f27d6cc2c318
madebychloe <chloechia0913@gmail.com>
2026-06-27 07:55:30 +0800

    Update tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -249,6 +249,11 @@ function recordCard(r) {
     </svg>Edit</button>` : '';
   const deleteBtn = editable ? `<button class="btn btn-danger btn-sm" onclick="confirmDeleteRecord('${r.recordId}')" title="Delete">
     <svg width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" style="margin-right:4px;vertical-align:middle"><polyline points="3 6 5 6 21 6"/><path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/><path d="M10 11v6"/><path d="M14 11v6"/><path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"/></svg>Delete</button>` : '';
+  const editBtnIcon = editable ? `<button class="card-icon-btn" onclick="openEditRecord('${r.recordId}')" title="Edit">
+    <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+      <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+    </svg></button>` : '';
   const deleteBtnIcon = editable ? `<button class="card-icon-btn card-icon-del" onclick="confirmDeleteRecord('${r.recordId}')" title="Delete">
     <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"/><path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/><path d="M10 11v6"/><path d="M14 11v6"/><path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"/></svg>
     </button>` : '';
